04eeee4dd795812c39c34b067a1939a8eedd2870
Amar Sharma <amarsharma.hacker@gmail.com>
2017-12-26 01:02:40 +0530

    Complete working pipeline

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
 # image-datapipeline
 
-Install PySpark from using `pip install pyspark`
+#Ideally in requirements.txt
 
-Run python pipeline.py INPUT_DIR OUTPUT_DIR
+Install PySpark from using
+`pip install pyspark`
+Install snakebite using
+`pip install snakebite`
 
+Size is hardcoded to 300 x 300
+
+Program assumes the Input directory is in hdfs and of structure
+`hdfs://INPUTDIR/CLASS/FILES.*`
+
+Output of program will be visible in
+`hdfs://OUTPUTDIR/CLASS/(training|testing|validation)/FILES.JPG`
+
+Locally Run using python pipeline.py INPUTDIR OUTPUTDIR [IP|localhost] HDFSPORT
 
--- pipeline.py
+++ pipeline.py
@@ -17,30 +17,54 @@ outputDir = sys.argv[2]
 
 size = 300, 300
 
+'''
+This function converts Binary data read from HDFS to image data for processing.
+Also rejecting malformed and invalid images.
+'''
+
 def toNPImage(data):
     try:
         return np.asarray(Image.open(StringIO(data)))
     except:
         return None
 
+'''
+This function does the transform and saving of file back to hdfs
+This can be optimized by partitioning data after the whole resizing is done.
+maybe a pipeline of first scaling and then paritioning into training, test
+and validation makes sense.
+
+Also assumptions are that the invalid and malformed files are way less then
+valid images to not screw up the calculation of paritions
+'''
+
 def transformAndSave(key, img):
     img = toNPImage(img)
     if img is not None:
-        filename = key.split("/")[-1]
         classname = key.split("/")[-2]
+        try:
+            list(client.ls([outputDir + '/' + classname]))
+        except Exception as ex:
+            for dirs in ['testing', 'validation', 'training']:
+                list(client.mkdir([outputDir + '/' + classname + '/' + dirs], True))
+        currentNumFiles = int(list(client.count([outputDir + '/' + classname]))[0]['fileCount'])
+        currentNumFiles += 1
+        totalInClass = int(list(client.count([inputDir + '/' + classname]))[0]['fileCount'])
+        filename = "img" + str(currentNumFiles) + ".jpg"
         rootDir = tmpOutDir
         directory = rootDir + '/' + classname
         if not os.path.exists(directory):
             os.makedirs(directory)
-        path = directory + '/' + key.split("/")[-1]
+        path = directory + '/' + filename
         im = Image.fromarray(img)
         im = im.resize(size, Image.ANTIALIAS)
         im.save(path)
-        try:
-            list(client.ls([outputDir + '/' + classname]))
-        except:
-            list(client.mkdir([outputDir + '/' + classname]))
-        put = Popen(["hadoop", "fs", "-put" , path, outputDir + '/' + classname + '/' + key.split("/")[-1]])
+        midDir = 'training'
+        if currentNumFiles * 100/totalInClass >= 70:
+            midDir = 'testing'
+        elif currentNumFiles * 100/totalInClass >= 90:
+            midDir = 'validation'
+        put = Popen(["hadoop", "fs", "-put" , path, outputDir + '/' + classname + '/' + midDir + '/'])
         put.communicate()
     return img
 
@@ -50,8 +74,8 @@ conf = (SparkConf()
          .setAppName("My app")
          .set("spark.executor.memory", "3g")
          .set('spark.driver.memory','15g'))
-sc = SparkContext(conf = conf)
 
+sc = SparkContext(conf = conf)
 images = sc.binaryFiles('hdfs://' + HDFS_SERVER + ':' + HDFS_PORT + '/' + inputDir+'/*')
 images.map(lambda (x, y): (x,transformAndSave(x, y))).collect()
 sc.stop()
