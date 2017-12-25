527cbe356ca7babec1a2d16e9180e46555e742a9
Amar Sharma <amarsharma.hacker@gmail.com>
2017-12-25 22:37:18 +0530

    Complete with hdfs

--- pipeline.py
+++ pipeline.py
@@ -2,12 +2,17 @@ from StringIO import StringIO
 from PIL import Image
 import numpy as np
 import os, sys
+from subprocess import Popen, PIPE
+from snakebite.client import Client
 
 if len(sys.argv) == 0:
-    print("Usage: python pipeline INPUT_DIR OUTPUT_DIR")
+    print("Usage: python pipeline INPUT_DIR OUTPUT_DIR HDFS_SERVER HDFS_PORT")
     exit(1)
-
+HDFS_SERVER = sys.argv[3]
+HDFS_PORT = sys.argv[4]
+client = Client(HDFS_SERVER, int(HDFS_PORT), use_trash=False)
 inputDir = sys.argv[1]
+tmpOutDir = '/tmp/output'
 outputDir = sys.argv[2]
 
 size = 300, 300
@@ -23,7 +28,7 @@ def transformAndSave(key, img):
     if img is not None:
         filename = key.split("/")[-1]
         classname = key.split("/")[-2]
-        rootDir = outputDir
+        rootDir = tmpOutDir
         directory = rootDir + '/' + classname
         if not os.path.exists(directory):
             os.makedirs(directory)
@@ -31,6 +36,12 @@ def transformAndSave(key, img):
         im = Image.fromarray(img)
         im = im.resize(size, Image.ANTIALIAS)
         im.save(path)
+        try:
+            list(client.ls([outputDir + '/' + classname]))
+        except:
+            list(client.mkdir([outputDir + '/' + classname]))
+        put = Popen(["hadoop", "fs", "-put" , path, outputDir + '/' + classname + '/' + key.split("/")[-1]])
+        put.communicate()
     return img
 
 from pyspark import SparkConf, SparkContext
@@ -41,5 +52,6 @@ conf = (SparkConf()
          .set('spark.driver.memory','15g'))
 sc = SparkContext(conf = conf)
 
-images = sc.binaryFiles(inputDir+'/*')
-imagerdd = images.map(lambda (x, y): (x,transformAndSave(x, y))).collect()
+images = sc.binaryFiles('hdfs://' + HDFS_SERVER + ':' + HDFS_PORT + '/' + inputDir+'/*')
+images.map(lambda (x, y): (x,transformAndSave(x, y))).collect()
+sc.stop()
